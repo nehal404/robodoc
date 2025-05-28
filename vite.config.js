32c87fb2a0085758bee3047b3b1f00a500571fd1
nehal <nehal.alaa@msa.edu.eg>
2025-05-28 14:23:26 +0300

    Rebuilt again

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,23 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/robodoc/', // Match your repository name
+  base: '/robodoc/',
   server: {
     open: true,
+    hmr: {
+      overlay: false,
+    },
   },
   build: {
     outDir: 'dist',
+    copyPublicDir: true,
+    rollupOptions: {
+      onLog(level, log, handler) {
+        if (log.message.includes('copying')) {
+          console.log(log.message); // Log file copying
+        }
+        handler(level, log);
+      },
+    },
   },
 });
